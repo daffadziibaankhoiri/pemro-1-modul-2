d78540befa35051288ce06061d912e5972b6b2c9
Daffa Dziiba'an Khoiri <155513300+daffadziibaankhoiri@users.noreply.github.com>
2025-10-19 18:03:47 +0800

    Add files via upload

---
+++ PRAK202-2510817210005-Daffa-Dziibaan-Khoiri.py
@@ -0,0 +1,4 @@
+input1 = input("Masukkan Nilai Pertama : ")
+input2 = input("Masukkan Nilai Kedua: ")
+hasil = float(input1) + float(input2)
+print(f'Hasil dari penjumlahan nilai pertama "{input1}" dan nilai kedua "{input2}" adalah "{hasil:.2f}"')
